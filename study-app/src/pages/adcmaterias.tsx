773467472dc34f07efbd9bd193f8bd07354ba026
Brsamuel <brsamuel360@gmail.com>
2024-10-03 15:40:19 -0300

    Brian Version, [Update adcmaterias, materias]

--- study-app/src/pages/adcmaterias.tsx
+++ study-app/src/pages/adcmaterias.tsx
@@ -1,63 +1,134 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, Alert } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Alert, TouchableOpacity } from 'react-native';
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { useNavigation,NavigationProp  } from '@react-navigation/native';
-import { style } from './login';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Adcmat = () => {
-    const [materia, setMateria]          = useState('');
-    const [loading,setLoading]           = useState(false);
+    const [materia, setMateria] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [date, setDate] = useState<Date | undefined>(undefined);
+    const [time, setTime] = useState<Date | undefined>(undefined);
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showTimePicker, setShowTimePicker] = useState(false);
 
     const navigation = useNavigation<NavigationProp<any>>();
-    
+
+    const handleDateChange = (event: any, selectedDate?: Date) => {
+        setShowDatePicker(false);
+        if (event.type === 'set') {
+            setDate(selectedDate);
+        }
+    };
+
+    const handleTimeChange = (event: any, selectedTime?: Date) => {
+        setShowTimePicker(false);
+        if (event.type === 'set') {
+            setTime(selectedTime);
+        }
+    };
+
     async function getMateria() {
         try {
-            setLoading(true)
-            if(!materia){
+            setLoading(true);
+            if (!materia) {
                 setLoading(false);
-                return Alert.alert('Materias não informadas!!')
+                return Alert.alert('Matérias não informadas!!');
             }
             navigation.navigate('AppRouter');
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Tela Materia</Text>
-      <Input 
-                    value={materia}
-                    onChangeText={setMateria}
-                    IconRigth={() => <AntDesign size={24} color="black" />}
-                    iconRightName="curso"
-                    onIconRigthPress={()=>console.log('MATERIA')}
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Tela Matéria</Text>
+            <Input
+                value={materia}
+                onChangeText={setMateria}
+                IconRigth={() => <AntDesign size={24} color="black" />}
+                iconRightName="curso"
+                onIconRigthPress={() => console.log('MATERIA')}
+            />
+
+            {/* Botão para selecionar a data */}
+            <TouchableOpacity style={styles.dateButton} onPress={() => setShowDatePicker(true)}>
+                <Text style={styles.dateButtonText}>
+                    {date ? date.toLocaleDateString() : "Selecionar Data"}
+                </Text>
+            </TouchableOpacity>
+
+            {/* Botão para selecionar a hora */}
+            <TouchableOpacity style={styles.dateButton} onPress={() => setShowTimePicker(true)}>
+                <Text style={styles.dateButtonText}>
+                    {time ? time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "Selecionar Hora"}
+                </Text>
+            </TouchableOpacity>
+
+            {/* Seletor de data */}
+            {showDatePicker && (
+                <DateTimePicker
+                    value={date || new Date()}
+                    mode="date"
+                    is24Hour={true}
+                    display="default"
+                    onChange={handleDateChange}
                 />
-        <View style={style.boxBottom}>
-            <Button  text="Próximo" loading={loading} onPress={()=>getMateria()}/>
+            )}
+
+            {/* Seletor de hora */}
+            {showTimePicker && (
+                <DateTimePicker
+                    value={time || new Date()}
+                    mode="time"
+                    is24Hour={true}
+                    display="default"
+                    onChange={handleTimeChange}
+                />
+            )}
+
+            <View style={styles.boxBottom}>
+                <Button text="Próximo" loading={loading} onPress={() => getMateria()} />
+            </View>
         </View>
-    </View>
-    
-  );
+    );
 };
 
 export default Adcmat;
 
 export const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-},
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  boxBottom: {
-    height:Dimensions.get('window').height/3,
-    width:'100%',
-    alignItems:'center',
-    justifyContent:'flex-start'
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    boxBottom: {
+        height: Dimensions.get('window').height / 3,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    dateButton: {
+        height: 40,
+        borderColor: '#007BFF',
+        borderWidth: 1,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20, // Aumentando o espaço entre os botões
+        marginTop: 20, // Aumentando o espaço entre o campo de texto e os botões
+        backgroundColor: '#FFF',
+        width: '80%',
+    },
+    dateButtonText: {
+        fontSize: 16,
+        color: '#333',
     },
 });
--- study-app/src/pages/materias.tsx
+++ study-app/src/pages/materias.tsx
@@ -1,64 +1,122 @@
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, Button, FlatList, TextInput } from 'react-native';
-
-interface Subject {
-  name: string;
-  schedule: string; // Você pode usar um formato mais complexo se necessário
-}
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert, Modal, StyleSheet, Animated } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Materias = () => {
-  const [subjects, setSubjects] = useState<Subject[]>([]);
-  const [name, setName] = useState('');
-  const [schedule, setSchedule] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [subjectName, setSubjectName] = useState('');
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [time, setTime] = useState<Date | undefined>(undefined); // Adicionando estado para o horário
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showTimePicker, setShowTimePicker] = useState(false); // Adicionando estado para exibir o seletor de horário
+  const [subjects, setSubjects] = useState<{ name: string; date: string; time: string }[]>([]);
 
   const handleAddSubject = () => {
-    if (!name || !schedule) {
-      alert('Por favor, preencha todos os campos.');
-      return;
+    if (subjectName && date && time) {
+      const newSubject = { 
+        name: subjectName, 
+        date: date.toISOString().split('T')[0], 
+        time: time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) 
+      };
+      setSubjects([...subjects, newSubject]);
+      setSubjectName('');
+      setDate(undefined);
+      setTime(undefined);
+      setIsAdding(false);
+    } else {
+      Alert.alert('Por favor, preencha todos os campos!');
     }
-
-    const newSubject: Subject = {
-      name,
-      schedule,
-    };
-
-    setSubjects([...subjects, newSubject]);
-    setName('');
-    setSchedule('');
-    alert('Matéria adicionada com sucesso!');
   };
 
-  const renderItem = ({ item }: { item: Subject }) => (
-    <View style={styles.subjectItem}>
-      <Text style={styles.subjectName}>{item.name}</Text>
-      <Text>Horário: {item.schedule}</Text>
-    </View>
-  );
+  const handleDeleteSubject = (index: number) => {
+    const updatedSubjects = subjects.filter((_, i) => i !== index);
+    setSubjects(updatedSubjects);
+  };
+
+  const handleDateChange = (event: any, selectedDate?: Date) => {
+    setShowDatePicker(false);
+    if (event.type === 'set') {
+      setDate(selectedDate);
+    }
+  };
+
+  const handleTimeChange = (event: any, selectedTime?: Date) => {
+    setShowTimePicker(false);
+    if (event.type === 'set') {
+      setTime(selectedTime);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Plano de Estudo</Text>
+      <Text style={styles.text}>Plano de Estudo</Text>
 
-      {/* Campos para adicionar nova matéria */}
-      <TextInput
-        style={styles.input}
-        placeholder="Nome da matéria"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Dias e horários das aulas"
-        value={schedule}
-        onChangeText={setSchedule}
-      />
-      <Button title="Adicionar Matéria" onPress={handleAddSubject} />
+      {isAdding && (
+        <View style={styles.formContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome da Matéria"
+            value={subjectName}
+            onChangeText={setSubjectName}
+          />
+          <TouchableOpacity style={styles.dateButton} onPress={() => setShowDatePicker(true)}>
+            <Text style={styles.dateButtonText}>
+              {date ? date.toLocaleDateString() : "Selecionar Data"}
+            </Text>
+          </TouchableOpacity>
 
-      {/* Lista de matérias */}
+          <TouchableOpacity style={styles.dateButton} onPress={() => setShowTimePicker(true)}>
+            <Text style={styles.dateButtonText}>
+              {time ? time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "Selecionar Hora"}
+            </Text>
+          </TouchableOpacity>
+
+          {showDatePicker && (
+            <DateTimePicker
+              value={date || new Date()}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              onChange={handleDateChange}
+            />
+          )}
+
+          {showTimePicker && (
+            <DateTimePicker
+              value={time || new Date()}
+              mode="time" // Modo para selecionar a hora
+              is24Hour={true}
+              display="default"
+              onChange={handleTimeChange}
+            />
+          )}
+
+          <TouchableOpacity style={styles.submitButton} onPress={handleAddSubject}>
+            <Text style={styles.submitButtonText}>Adicionar Matéria</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      <TouchableOpacity style={styles.addButton} onPress={() => setIsAdding(!isAdding)}>
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+
       <FlatList
         data={subjects}
-        renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.subjectContainer}>
+            <Text style={styles.subjectText}>{item.name}</Text>
+            <View style={styles.subjectDetails}>
+              <Text style={styles.subjectDetailText}>Data: {item.date}</Text>
+              <Text style={styles.subjectDetailText}>Hora: {item.time}</Text>
+              <TouchableOpacity onPress={() => handleDeleteSubject(index)} style={styles.deleteButton}>
+                <Icon name="delete" size={24} color="#FF0000" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
         style={styles.subjectList}
       />
     </View>
@@ -68,36 +126,101 @@ const Materias = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     backgroundColor: '#353535',
+    paddingTop: 20,
   },
-  title: {
-    fontSize: 28,
+  text: {
+    fontSize: 24,
     fontWeight: 'bold',
     color: '#FFF',
     marginBottom: 20,
-    alignSelf: 'center',
+  },
+  formContainer: {
+    marginTop: 20,
+    width: '80%',
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
   },
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 15,
-    paddingHorizontal: 10,
-    backgroundColor: '#FFF',
-  },
-  subjectItem: {
-    padding: 15,
-    backgroundColor: '#FFF',
     marginBottom: 10,
-    borderRadius: 5,
+    paddingLeft: 10,
   },
-  subjectName: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  dateButton: {
+    height: 40,
+    borderColor: '#007BFF',
+    borderWidth: 1,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#FFF',
+  },
+  dateButtonText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  addButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#2196F3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  addButtonText: {
+    fontSize: 30,
+    color: '#FFF',
   },
   subjectList: {
     marginTop: 20,
+    width: '80%',
+  },
+  subjectContainer: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 10,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  subjectText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subjectDetails: {
+    marginTop: 5,
+  },
+  subjectDetailText: {
+    fontSize: 14,
+    color: '#555',
+  },
+  deleteButton: {
+    marginLeft: 10,
+  },
+  submitButton: {
+    backgroundColor: '#2196F3',
+    borderRadius: 8,
+    padding: 15,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  submitButtonText: {
+    color: '#FFF',
+    fontSize: 16,
   },
 });
 
